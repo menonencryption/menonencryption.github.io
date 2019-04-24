11b57d09571684f703a6d24d32130b447cd9fa95
OneStig <therealonestig@gmail.com>
2019-04-23 17:43:22 -0700

    Brute force cipher

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+var bruteIndex = 1;
+var bruteInvert = false;
+var brutedReg = [];
+var brutedInv = [];
+
 function rot(str, i) {
   return str.replace(/[a-zA-Z]/g, function(c) {
     return String.fromCharCode((c <= 'Z' ? 90 : 122) >= (c = c.charCodeAt(0) + i) ? c : c - 26);
@@ -11,4 +16,54 @@ function reverse(s){
 function update() {
   document.getElementById('outputEncrypt').value = reverse(rot(document.getElementById('inputEncrypt').value, 10));
   document.getElementById('outputDecrypt').value = rot(reverse(document.getElementById('inputDecrypt').value), 16);
+}
+
+function bruteForce() {
+  var input = document.getElementById('inputBruteForce').value;
+  brutedReg = [];
+  brutedInv = [];
+  for (var i = 1; i < 27; i++) {
+    brutedReg.push(rot(input, i));
+    brutedInv.push(reverse(rot(input, i)));
+  }
+
+  var label = "ROT" + Math.floor(bruteIndex).toString();
+
+  if (!bruteInvert) {
+    label = label + " Not Inverted";
+    document.getElementById('outputBruteForce').value = brutedReg[bruteIndex];
+  }
+  else {
+    label = label + " Inverted";
+    document.getElementById('outputBruteForce').value = brutedInv[bruteIndex];
+  }
+
+  document.getElementById('outputBruteForceLabel').value = label;
+}
+
+function next() {
+  if (bruteIndex >= 25) {
+    bruteIndex = 1;
+  }
+  else {
+    if (bruteInvert) {
+      bruteIndex++;
+    }
+  }
+  bruteInvert = !bruteInvert;
+
+  bruteForce();
+}
+
+function prev() {
+  if (bruteIndex <= 1) {
+    bruteIndex = 25;
+  }
+  else {
+    if (!bruteInvert) {
+      bruteIndex--;
+    }
+  }
+  bruteInvert = !bruteInvert;
+  bruteForce();
 }
